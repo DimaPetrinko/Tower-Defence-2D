12d2aa46f3c18efda8a962d4656afd98cb95801f
Blind56 <petrinko6@gmail.com>
2015-07-22 22:22:04 +0300

    Basic Scene Setup

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	public float horizontalSpeed = 0.5f;
+	public float verticalSpeed = 0.25f;
+
+	void FixedUpdate () {
+		transform.Translate(Vector3.up * Input.GetAxis("Vertical") * verticalSpeed);
+		transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * horizontalSpeed);
+	}
+}
